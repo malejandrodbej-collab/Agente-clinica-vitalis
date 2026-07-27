7f467f6155dd4b5e4f6dff9cdcabdd9f7cc5ec31
malejandrodbej-collab <malejandro.dbej@gmail.com>
2026-07-27 00:25:33 -0600

    Eliminar scripts de diagnóstico y versiones ya no usadas

--- agente-documental/src/diagnostico.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("../data/clinica_vitalis_documentacion.pdf")
-docs = loader.load()
-texto_completo = "\n".join(d.page_content for d in docs)
-idx = texto_completo.lower().find("embarazo")
-print(texto_completo[max(0, idx-200):idx+300])
